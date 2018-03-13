4a7ee07592165ccd0eee5ba9822c535115cf7f00
dmitrykazak <dmitry.kazak@triboo.it>
2018-03-13 23:06:59 +0300

    added resource for customer ip

--- app/code/community/DK/CustomerIP/sql/dk_customerip_setup/install-1.0.0.php
+++ app/code/community/DK/CustomerIP/sql/dk_customerip_setup/install-1.0.0.php
@@ -1,2 +1,14 @@
 <?php
 
+/** @var Mage_Eav_Model_Entity_Setup $installer */
+$installer = $this;
+$installer->startSetup();
+
+$installer->addAttribute('customer', 'registration_ip', [
+    'type' => 'varchar',
+    'label' => 'Customer\'s IP at registration',
+    'visible' => true,
+    'required' => false,
+]);
+
+$installer->endSetup();
