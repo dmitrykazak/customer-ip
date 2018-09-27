2080b327ea4606626173b2d7965b4517a93a8af0
dmitrykazak <Demon1k@tut.by>
2018-09-27 15:59:15 +0300

    normalize fields an values

---
+++ app/code/community/DK/CustomerIP/Helper/Normalizer.php
@@ -0,0 +1,30 @@
+<?php
+
+class DK_CustomerIP_Helper_Normalizer extends Mage_Core_Helper_Abstract
+{
+    /**
+     * @var array
+     */
+    protected $skipValues = [
+        true,
+        false,
+    ];
+
+    /**
+     * @param array $data
+     *
+     * @return array
+     */
+    public function normalize(array $data)
+    {
+        $normalizerData = [];
+        foreach ($data as $name => $value) {
+            if (!in_array($value, $this->skipValues, true)) {
+                $normalizerField = ucfirst(str_replace('_', ' ', $name));
+                $normalizerData[$normalizerField] = $value;
+            }
+        }
+
+        return $normalizerData;
+    }
+}
